4c891f1824dfe90f1c1ec765c75853b024e86295
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-02-23 13:38:07 +0300

    Add files via upload

--- configs.py
+++ configs.py
@@ -1,4 +1,15 @@
 import argparse
+import logging
+from logging.handlers import RotatingFileHandler
+
+from constants import BASE_DIR
+
+
+# Время записи – Уровень сообщения – Cообщение.
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+# Указываем формат времени.
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+
 
 def configure_argument_parser(available_modes):
     parser = argparse.ArgumentParser(description='Парсер документации Python')
@@ -9,4 +20,25 @@ def configure_argument_parser(available_modes):
                         '--clear-cache',
                         action='store_true',
                         help='Очистка кеша')
-    return parser 
+    parser.add_argument('-o',
+                        '--output',
+                        choices=('pretty', 'file'),
+                        help='Дополнительные способы вывода данных')
+    return parser
+
+def configure_logging():
+    log_dir = BASE_DIR / 'logs'
+    log_dir.mkdir(exist_ok=True)
+    log_file = log_dir / 'parser.log'
+    # Инициализация хендлера с ротацией логов.
+    # Максимальный объём одного файла — десять в шестой степени байт (10**6), 
+    # максимальное количество файлов с логами — 5.
+    rotating_handler = RotatingFileHandler(log_file,
+                                           maxBytes=10 **6,
+                                           backupCount=5)
+    # Базовая настройка логирования basicConfig.
+    logging.basicConfig(datefmt=DT_FORMAT,
+                        format=LOG_FORMAT,
+                        level=logging.INFO,
+                        handlers=(rotating_handler,
+                                  logging.StreamHandler()))
